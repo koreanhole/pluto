511a542514002710a30f736262c86fb00265b8e1
SoonHyeong Kwon <koreanhole1@gmail.com>
2020-07-30 00:55:28 +0900

    [global-app] Delete Drawer

--- src/navigators/AppNavigator.tsx
+++ src/navigators/AppNavigator.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-// import { createDrawerNavigator } from "@react-navigation/drawer";
 import Home from "../components/Home";
-// import Drawer from "../modules/Drawer";
 
 export type RootStackParamList = {
   Home: undefined;
@@ -12,17 +10,9 @@ export type RootStackParamList = {
 };
 
 const StackNavigator = createStackNavigator<RootStackParamList>();
-// const DrawerNavigator = createDrawerNavigator();
-
-// const DrawerNavigation = () => (
-//   <DrawerNavigator.Navigator drawerContent={(props) => <Drawer {...props} />}>
-//     <DrawerNavigator.Screen name="HOME" component={Home} />
-//   </DrawerNavigator.Navigator>
-// );
 
 const PageNavigation = () => (
   <StackNavigator.Navigator headerMode="none">
-    {/* <StackNavigator.Screen name="Drawer" component={DrawerNavigation} /> */}
     <StackNavigator.Screen name="Home" component={Home} />
   </StackNavigator.Navigator>
 );
