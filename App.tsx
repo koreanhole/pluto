298246fae80847665d2a243210d7298b8ca0bab9
SoonHyeong Kwon <koreanhole1@gmail.com>
2020-08-21 12:21:48 +0900

    [app] redux persist 다시 추가

--- App.tsx
+++ App.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import { Provider } from "react-redux";
 import AppNavigator from "navigators/AppNavigator";
 import createReduxStore from "redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 
-const store = createReduxStore();
+const { store, persistor } = createReduxStore();
 
 export default function App() {
   return (
     <Provider store={store}>
-      <ActionSheetProvider>
-        <AppNavigator />
-      </ActionSheetProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ActionSheetProvider>
+          <AppNavigator />
+        </ActionSheetProvider>
+      </PersistGate>
     </Provider>
   );
 }
