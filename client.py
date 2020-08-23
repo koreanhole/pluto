cfcf56f02fda1054022cfd8220b0834d27930770
SoonHyeong Kwon <koreanhole1@gmail.com>
2020-08-23 18:30:40 +0900

    [server] add push notification send class

--- client.py
+++ client.py
@@ -7,6 +7,13 @@ from util import GeneralClassification, EngineeringClassification, EconomicsClas
 import ssl
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
+from exponent_server_sdk import DeviceNotRegisteredError
+from exponent_server_sdk import PushClient
+from exponent_server_sdk import PushMessage
+from exponent_server_sdk import PushResponseError
+from exponent_server_sdk import PushServerError
+from requests.exceptions import ConnectionError
+from requests.exceptions import HTTPError
 
 cred = credentials.Certificate('./ServiceAccountKey.json')
 firebase_admin.initialize_app(cred)
@@ -68,5 +75,55 @@ class FirebaseUpload(object):
             cls.uploadSingleNotice(deptCode)
 
 
-FirebaseUpload.uploadMultiNotice()
+class ExpoPushNotification(object):
+    @classmethod
+    def send_push_message(cls, token, title, message, extra=None):
+        try:
+            response = PushClient().publish(
+                PushMessage(to=token,
+                            title=title,
+                            body=message,
+                            data=extra))
+        except PushServerError as exc:
+            # Encountered some likely formatting/validation error.
+            rollbar.report_exc_info(
+                extra_data={
+                    'token': token,
+                    'title': title,
+                    'message': message,
+                    'extra': extra,
+                    'errors': exc.errors,
+                    'response_data': exc.response_data,
+                })
+            raise
+        except (ConnectionError, HTTPError) as exc:
+            # Encountered some Connection or HTTP error - retry a few times in
+            # case it is transient.
+            rollbar.report_exc_info(
+                extra_data={'token': token, 'title': title, 'message': message, 'extra': extra})
+            raise self.retry(exc=exc)
+
+        try:
+            # We got a response back, but we don't know whether it's an error yet.
+            # This call raises errors so we can handle them with normal exception
+            # flows.
+            response.validate_response()
+        except DeviceNotRegisteredError:
+            # Mark the push token as inactive
+            from notifications.models import PushToken
+            PushToken.objects.filter(token=token).update(active=False)
+        except PushResponseError as exc:
+            # Encountered some other per-notification error.
+            rollbar.report_exc_info(
+                extra_data={
+                    'token': token,
+                    'title': title,
+                    'message': message,
+                    'extra': extra,
+                    'push_response': exc.push_response._asdict(),
+                })
+            raise self.retry(exc=exc)
+
+
+# FirebaseUpload.uploadMultiNotice()
 # FirebaseUpload.saveLastVisitedListId("20013DA1", "11899")
