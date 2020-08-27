0c6bd8ce9e9a1992aec612c21b2dc1f92be8f69f
SoonHyeong Kwon <koreanhole1@gmail.com>
2020-08-28 04:36:34 +0900

    [app] ignore android timer warning

--- app/App.tsx
+++ app/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { YellowBox } from "react-native";
 import { Provider } from "react-redux";
 import AppNavigator from "navigators/AppNavigator";
 import createReduxStore from "redux/store";
@@ -8,6 +9,8 @@ import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 const { store, persistor } = createReduxStore();
 
 export default function App() {
+  //https://github.com/facebook/react-native/issues/12981
+  YellowBox.ignoreWarnings(["Setting a timer"]);
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
