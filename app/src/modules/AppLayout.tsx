c7229369bdb50f4c921017250c457658b4a9817f
SoonHyeong Kwon <koreanhole1@gmail.com>
2020-08-28 20:44:45 +0900

    [app] delete expo statusbar

--- app/src/modules/AppLayout.tsx
+++ app/src/modules/AppLayout.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { View } from "react-native";
 import styled from "styled-components/native";
-import { StatusBar } from "expo-status-bar";
 
 type AppLayoutProps = {
   children?: React.ReactNode;
@@ -18,7 +17,6 @@ export default function AppLayout(props: AppLayoutProps) {
     <React.Fragment>
       <View style={[{ flex: 1 }, { backgroundColor: "#fff" }]}>
         <MainSection>{children}</MainSection>
-        <StatusBar style="auto" />
       </View>
     </React.Fragment>
   );
