923a430894bba17270013cc69cc3db4625d4ebfe
SoonHyeong Kwon <koreanhole1@gmail.com>
2020-09-18 00:09:04 +0900

    [app] 상태바 검정색으로 나오게 설정

--- app/src/modules/AppLayout.tsx
+++ app/src/modules/AppLayout.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
-import { View } from "react-native";
+import { View, StatusBar } from "react-native";
 import styled from "styled-components/native";
+import theme from "theme";
 
 type AppLayoutProps = {
   children?: React.ReactNode;
@@ -16,6 +17,10 @@ export default function AppLayout(props: AppLayoutProps) {
   return (
     <React.Fragment>
       <View style={[{ flex: 1 }, { backgroundColor: "#fff" }]}>
+        <StatusBar
+          backgroundColor={theme.colors.ligthGrey}
+          barStyle="dark-content"
+        />
         <MainSection>{children}</MainSection>
       </View>
     </React.Fragment>
