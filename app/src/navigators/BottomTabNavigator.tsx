7778428112ebd274d7aa4e1d93fd7d561ef21fef
SoonHyeong Kwon <koreanhole1@gmail.com>
2020-10-03 11:02:23 +0900

    [app] 하단바 변경

--- app/src/navigators/BottomTabNavigator.tsx
+++ app/src/navigators/BottomTabNavigator.tsx
@@ -1,31 +1,37 @@
-import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import * as React from "react";
 import HomeStackNavigator from "./stack/HomeStackNavigator";
 import FavoriteStackNavigator from "./stack/FavoriteStackNavigator";
 import theme from "theme";
 import { MaterialIcons, MaterialCommunityIcons } from "@expo/vector-icons";
 
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function BottomTabNavigator() {
   return (
     <Tab.Navigator
       initialRouteName="Home"
-      shifting={true}
-      activeColor={theme.colors.primary}
-      inactiveColor={theme.colors.black}
-      sceneAnimationEnabled={true}
+      tabBarOptions={{
+        activeTintColor: theme.colors.primary,
+        style: {
+          backgroundColor: theme.colors.ligthGrey,
+        },
+        showLabel: false,
+      }}
     >
       <Tab.Screen
         name="Home"
         component={HomeStackNavigator}
         options={{
           tabBarLabel: "홈",
-          tabBarColor: theme.colors.ligthGrey,
-          tabBarIcon: ({ focused, color }) => {
+          tabBarIcon: ({ focused, color, size }) => {
             let iconName = focused ? "home" : "home-outline";
             return (
-              <MaterialCommunityIcons name={iconName} size={25} color={color} />
+              <MaterialCommunityIcons
+                name={iconName}
+                size={size}
+                color={color}
+              />
             );
           },
         }}
@@ -35,10 +41,9 @@ export default function BottomTabNavigator() {
         component={FavoriteStackNavigator}
         options={{
           tabBarLabel: "저장",
-          tabBarColor: theme.colors.ligthGrey,
-          tabBarIcon: ({ focused, color }) => {
+          tabBarIcon: ({ focused, color, size }) => {
             let iconName = focused ? "favorite" : "favorite-border";
-            return <MaterialIcons name={iconName} size={25} color={color} />;
+            return <MaterialIcons name={iconName} size={size} color={color} />;
           },
         }}
       />
