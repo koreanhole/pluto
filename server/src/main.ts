f4f21e47caff5fcb66a6910c8f8ab0321e4138d0
koreanhole <koreanhole1@gmail.com>
2021-02-07 00:21:01 +0900

    add logger for main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,15 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
+
   const app = await NestFactory.create(AppModule);
+
+  const port = 3000;
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
