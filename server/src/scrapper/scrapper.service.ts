009469322c66149fe7900c50a2c3ae347ace5012
koreanhole <koreanhole1@gmail.com>
2021-02-09 23:19:26 +0900

    [server] inject notice service and department service into scrapper service

--- server/src/scrapper/scrapper.service.ts
+++ server/src/scrapper/scrapper.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { DepartmentService } from '../department/department.service';
+import { NoticeService } from '../notice/notice.service';
 
 @Injectable()
-export class ScrapperService {}
+export class ScrapperService {
+  constructor(
+    private departmentService: DepartmentService,
+    private noticeService: NoticeService,
+  ) {}
+}
