912e435a5f67952888fea4228afa4ba120ebd1d9
koreanhole <koreanhole1@gmail.com>
2021-02-17 01:14:45 +0900

    [server] add environment config

--- server/src/config/typeorm.config.ts
+++ server/src/config/typeorm.config.ts
@@ -2,11 +2,14 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Department } from '../department/department.entity';
 import { Notice } from '../notice/notice.entity';
 import { User } from '../user/user.entity';
+import * as config from 'config';
+
+const dbConfig = config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'mongodb',
-  url: 'mongodb://localhost/notice',
-  synchronize: true,
+  type: dbConfig.type,
+  url: dbConfig.url,
+  synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
   useUnifiedTopology: true,
   entities: [Notice, Department, User],
 };
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,14 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
+  const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
-
   const app = await NestFactory.create(AppModule);
 
-  const port = 3001;
+  const port = process.env.PORT || serverConfig.port;
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
