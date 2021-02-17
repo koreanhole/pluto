3cf9a04c02694210a46d83c9842e9d51f847b867
koreanhole <koreanhole1@gmail.com>
2021-02-18 00:15:21 +0900

    [server] set up config

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,20 +1,22 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as helmet from 'helmet';
+// import * as helmet from 'helmet';
+import * as config from 'config';
 
 async function bootstrap() {
+  const serverConfig = config.get('server');
   const logger = new Logger('Bootstrap');
 
   const app = await NestFactory.create(AppModule);
-  app.use(
-    helmet({
-      contentSecurityPolicy:
-        process.env.NODE_ENV === 'production' ? undefined : false,
-    }),
-  );
-  app.enableCors();
-  const port = 3001;
+  // app.use(
+  //   helmet({
+  //     contentSecurityPolicy:
+  //       process.env.NODE_ENV === 'production' ? undefined : false,
+  //   }),
+  // );
+  // app.enableCors();
+  const port = process.env.PORT || serverConfig.port;
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
