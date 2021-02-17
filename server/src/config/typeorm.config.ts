7eff07b8acb61e5475b29673b85e75eb2aa0bab9
koreanhole <koreanhole1@gmail.com>
2021-02-18 01:06:41 +0900

    [server] add new Procfile

--- server/src/config/typeorm.config.ts
+++ server/src/config/typeorm.config.ts
@@ -1,8 +1,5 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as config from 'config';
-import { Department } from 'src/department/department.entity';
-import { Notice } from 'src/notice/notice.entity';
-import { User } from 'src/user/user.entity';
 
 const dbConfig = config.get('db');
 
@@ -13,7 +10,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   username: process.env.RDS_USERNAME || dbConfig.username,
   password: process.env.RDS_PASSWORD || dbConfig.password,
   database: process.env.RDS_DB_NAME || dbConfig.database,
-  entities: [Department, User, Notice],
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
   keepConnectionAlive: dbConfig.keepConnectionAlive,
 };
