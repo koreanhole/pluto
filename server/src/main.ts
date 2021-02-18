10d6fe9fe07b524233bb86153a15967951dad76f
koreanhole <koreanhole1@gmail.com>
2021-02-18 12:31:54 +0900

    [server] enable cors, helmet

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,7 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import * as helmet from 'helmet';
+import * as helmet from 'helmet';
 import * as config from 'config';
 
 async function bootstrap() {
@@ -9,13 +9,13 @@ async function bootstrap() {
   const logger = new Logger('Bootstrap');
 
   const app = await NestFactory.create(AppModule);
-  // app.use(
-  //   helmet({
-  //     contentSecurityPolicy:
-  //       process.env.NODE_ENV === 'production' ? undefined : false,
-  //   }),
-  // );
-  // app.enableCors();
+  app.use(
+    helmet({
+      contentSecurityPolicy:
+        process.env.NODE_ENV === 'production' ? undefined : false,
+    }),
+  );
+  app.enableCors();
   const port = process.env.PORT || serverConfig.port;
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
