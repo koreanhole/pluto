ba198c4511f9a9ef20be3b34a7ba789731306595
koreanhole <koreanhole1@gmail.com>
2021-02-26 01:32:59 +0900

    [server] auto create schemafile

--- server/src/config/graphql.config.ts
+++ server/src/config/graphql.config.ts
@@ -1,7 +1,8 @@
 import { GqlModuleOptions } from '@nestjs/graphql';
+import { join } from 'path';
 
 export const graphqlConfig: GqlModuleOptions = {
-  autoSchemaFile: true,
+  autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
   sortSchema: true,
   path: '/api',
 };
