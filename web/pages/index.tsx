921a2316847392a259f2686f87e5402303bebbcf
koreanhole <koreanhole1@gmail.com>
2021-04-25 22:38:03 +0900

    refactor notice card

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,11 +1,36 @@
 import AppLayout from "~/modules/AppLayout";
 import * as React from "react";
 import NoticeCard from "~/components/NoticeCard";
+import { NoticeCardData } from "~/components/types";
+
+const MOCK_ITEM: NoticeCardData = {
+  department: "컴퓨터과학부",
+  noticaCardListData: [
+    {
+      id: 1,
+      title: "test1",
+      author: "누굴까",
+      createdDateTime: "2020-07-21",
+    },
+    {
+      id: 2,
+      title: "test2",
+      author: "누굴까",
+      createdDateTime: "2020-07-22",
+    },
+    {
+      id: 3,
+      title: "test3",
+      author: "누굴까",
+      createdDateTime: "2020-07-23",
+    },
+  ],
+};
 
 export default function Home() {
   return (
     <AppLayout title="sample">
-      <NoticeCard />
+      <NoticeCard department={MOCK_ITEM.department} noticaCardListData={MOCK_ITEM.noticaCardListData} />
     </AppLayout>
   );
 }
--- web/src/components/NoticeCard.tsx
+++ web/src/components/NoticeCard.tsx
@@ -1,65 +1,38 @@
 import * as React from "react";
-import {
-  Avatar,
-  Card,
-  CardContent,
-  CardHeader,
-  createStyles,
-  Divider,
-  makeStyles,
-  Typography,
-  Box,
-} from "@material-ui/core";
-import { styled, Theme } from "@material-ui/core/styles";
+import { Avatar, Card, CardContent, CardHeader, Divider, Typography } from "@material-ui/core";
+import { styled } from "@material-ui/core/styles";
 import { getDescriptiveDateDifference } from "~/utils/time";
+import { NoticeCardListData, NoticeCardData } from "./types";
 
-const useStyles = makeStyles({
-  container: {},
-  itemInfoContainer: {
-    display: "flex",
-    flexDirection: "row",
-  },
+const NoticeCardContainer = styled(Card)({
+  maxWidth: "18rem",
+  marginTop: "2rem",
+  marginLeft: "2rem",
 });
 
 const CardItemInfoContainer = styled("div")({
   display: "flex",
   flexDirection: "row",
+  justifyContent: "space-between",
 });
 
 const CardItemInfoItem = styled(Typography)({
   fontSize: "1rem",
 });
 
-const MOCK_ITEM = [
-  {
-    id: 1,
-    title: "test1",
-    createdDateTime: "2020-07-21",
-  },
-  {
-    id: 2,
-    title: "test2",
-    createdDateTime: "2020-07-22",
-  },
-  {
-    id: 3,
-    title: "test3",
-    createdDateTime: "2020-07-23",
-  },
-];
-
-function NoticeCardHeader() {
-  return <CardHeader avatar={<Avatar aria-label="recipe">컴과</Avatar>} title="컴퓨터과학부" subheader="공과대학" />;
+function NoticeCardHeader({ department }: { department: string }) {
+  return <CardHeader avatar={<Avatar aria-label="recipe">컴과</Avatar>} title={department} subheader="공과대학" />;
 }
 
-function NoticeCardItemList() {
+function NoticeCardItemList({ data }: { data: NoticeCardListData[] }) {
   return (
     <CardContent>
-      {MOCK_ITEM.map((item) => (
+      {data.map((item) => (
         <React.Fragment key={item.id}>
+          <Typography>{`#${getDescriptiveDateDifference(item.createdDateTime)}`}</Typography>
           <Typography>{item.title}</Typography>
           <CardItemInfoContainer>
-            <CardItemInfoItem>{item.createdDateTime}</CardItemInfoItem>
+            <CardItemInfoItem>{item.author}</CardItemInfoItem>
             <CardItemInfoItem>{item.createdDateTime}</CardItemInfoItem>
           </CardItemInfoContainer>
           <Divider />
@@ -69,16 +42,12 @@ function NoticeCardItemList() {
   );
 }
 
-function NoticeCardFooter() {
-  return <></>;
-}
-
-export default function NoticeCard() {
+export default function NoticeCard(data: NoticeCardData) {
+  const { department, noticaCardListData } = data;
   return (
-    <Card>
-      <NoticeCardHeader />
-      <NoticeCardItemList />
-      <NoticeCardFooter />
-    </Card>
+    <NoticeCardContainer>
+      <NoticeCardHeader department={department} />
+      <NoticeCardItemList data={noticaCardListData} />
+    </NoticeCardContainer>
   );
 }
