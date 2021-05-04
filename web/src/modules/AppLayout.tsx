0d67f694c5582a5152caa2b9d1175015998d4447
koreanhole <koreanhole1@gmail.com>
2021-05-05 01:43:09 +0900

    [web] add download button in toolbar

--- web/src/modules/AppLayout.tsx
+++ web/src/modules/AppLayout.tsx
@@ -4,6 +4,8 @@ import styled from "styled-components";
 import { useRouter } from "next/router";
 import { AttachmentLinksType } from "src/components/types";
 import CloudDownload from "@material-ui/icons/CloudDownload";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 interface AppLayoutProps {
   title: string;
@@ -20,9 +22,18 @@ export default function AppLayout(props: AppLayoutProps) {
   const { title, children, attachmentLinks } = props;
 
   const router = useRouter();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
   const handleClickHome = () => {
     router.push("/");
+  };
+
+  const handleClickAttachment = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseAttachment = () => {
+    setAnchorEl(null);
   };
 
   return (
@@ -33,9 +44,27 @@ export default function AppLayout(props: AppLayoutProps) {
             <Typography variant="h6">{title}</Typography>
           </AppBarHomeText>
           {typeof attachmentLinks !== "undefined" && (
-            <IconButton color="inherit" aria-label="download">
-              <CloudDownload />
-            </IconButton>
+            <>
+              <IconButton color="inherit" aria-label="download" onClick={handleClickAttachment}>
+                <CloudDownload />
+              </IconButton>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleCloseAttachment}>
+                {attachmentLinks.map((data) => (
+                  <MenuItem
+                    onClick={() => {
+                      router.push(`${data.fileLink}`);
+                    }}
+                    key={data.fileLink}>
+                    {data.fileName}
+                  </MenuItem>
+                ))}
+              </Menu>
+            </>
           )}
         </Toolbar>
       </AppBar>
